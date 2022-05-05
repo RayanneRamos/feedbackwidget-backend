513ed8b0adc9b8bd2277facd0eac4d26c11b9ceb
Rayanne Ramos <rayanneramosoficial@gmail.com>
2022-05-05 15:20:25 -0300

    Criando o envio de email da aplicação

---
+++ src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import { MailAdapter, SendMailData } from "../mail-adapter";
+import nodemailer from 'nodemailer';
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "ca91599b4a3df8",
+    pass: "0c93b606be6647"
+  }
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: 'Equipe Feedget <oi@feedget.com>',
+      to: 'Rayanne Ramos<rayanneramosoficial@gmail.com>',
+      subject,
+      html: body,
+    });
+  }
+}
